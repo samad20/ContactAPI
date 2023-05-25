4e0ec8f807ca09a7c5455c38c53414265cb9a508
samad20 <musab.aljahdhami@gmail.com>
2023-05-25 10:14:06 +0400

    init DbContext class

---
+++ ContactAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ContactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Act as Table of Data Base
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
