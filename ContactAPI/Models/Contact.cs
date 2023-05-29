2572f169ed70e648d58a4bc76c18f8ba28cc3f4d
samad20 <musab.aljahdhami@gmail.com>
2023-05-29 12:50:49 +0400

    change Phone prop type to long

--- ContactAPI/Models/Contact.cs
+++ ContactAPI/Models/Contact.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; } 
         public string Email { get; set; }
-        public string Phone { get; set; }
+        public long Phone { get; set; }
         public string Address { get; set; }
     }
 
