76480746b6b81e007e7e611209b73cfdbc6bafe2
samad20 <musab.aljahdhami@gmail.com>
2023-05-29 12:46:58 +0400

    change phone prop to long

--- ContactAPI/Models/AddContactRequest.cs
+++ ContactAPI/Models/AddContactRequest.cs
@@ -4,7 +4,7 @@
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
+        public long Phone { get; set; }
         public string Address { get; set; }
     }
 }
